40b9f954e86d7c49120bb28f89796e115e5cdb8d
Gustavo Barbosa <44094756+gusbdev@users.noreply.github.com>
2022-07-27 21:23:13 -0300

    Update api.ts
    Co-authored-by: Yan Gabriel yangabriel2012@gmail.com

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios, {AxiosInstance} from "axios"
 
 const api: AxiosInstance = axios.create({
-  baseURL: "http://api.openweathermap.org/data/2.5/weather?q=",
-  
+  baseURL: "http://api.openweathermap.org/data/2.5/weather?q="
 })
 
 export default api
